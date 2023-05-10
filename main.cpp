007b3eecfcba69e99098ed6764662693562b6298
mrutkowski-aero <mrutkowski@meil.pw.edu.pl>
2023-05-10 11:56:22 +0200

    Update main.cpp
    Language changed, comments added

--- main.cpp
+++ main.cpp
@@ -6,26 +6,26 @@
 void main() {
 
 
-	int projekt, wybrany;
-	int L = 14;
-	int projekty[13];
+	int proj, chosen;
+	int L = 14; // Amount of projects
+	int list[13]; // Number of students
 
-	srand(time(NULL));          //inicjalizacja generatora
+	srand(time(NULL));
 
 	for (int i = 0; i < 13; i++)
 	{
 		do {
-			wybrany = 1;
-			projekt = rand() % L + 1;
-			for (int j = 0; j < i; j++)
-				if (projekty[j] == projekt) 
-					wybrany = 0;
-			if (wybrany == 1) projekty[i] = projekt;
-		} while (wybrany != 1);
+			chosen = 1;
+			proj = rand() % L + 1;
+			for (int j = 0; j < i; j++) //check if project is already generated
+				if (list[j] == proj)
+					chosen = 0;
+			if (chosen == 1) list[i] = proj; // Project number is unique, give it to i'th student
+		} while (chosen != 1); // Until generated project is not unique - iterate
 	}
 
-
-	for (int i = 0; i < 13; i++) printf(" Wylosowana liczba to %d\n", projekty[i]);
+	printf("Students number \t Project number")
+	for (int i = 0; i < 13; i++) printf("%3.0d \t %3.0d\n", i,list[i]);
 
 
 }
